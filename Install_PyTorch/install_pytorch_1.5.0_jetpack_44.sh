37d4712a7e4106cda055254b0699ac17f2589dad
JinFree <ytrqwe12@gmail.com>
2020-06-16 21:09:35 +0900

    fix bug

--- Install_PyTorch/install_pytorch_1.5.0_jetpack_44.sh
+++ Install_PyTorch/install_pytorch_1.5.0_jetpack_44.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd 
 wget https://nvidia.box.com/shared/static/3ibazbiwtkl181n95n9em3wtrca7tdzp.whl -O torch-1.5.0-cp36-cp36m-linux_aarch64.whl
-sudo apt-get install python3-pip libopenblas-base libopenmpi-dev 
+sudo apt-get install python3-pip libopenblas-base libopenmpi-dev -y
 pip3 install Cython
 pip3 install numpy torch-1.5.0-cp36-cp36m-linux_aarch64.whl
 
-sudo apt-get install libjpeg-dev zlib1g-dev
+sudo apt-get install libjpeg-dev zlib1g-dev -y
 git clone --branch v0.6.0 https://github.com/pytorch/vision torchvision   # see below for version of torchvision to download
 cd torchvision
 sudo python3 setup.py install
---
+++ basic_setup_for_nano.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt install python-dev python3-dev python-pip python3-pip -y
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda/lib64" >> ~/.bashrc
+echo "export PATH=\$PATH:/usr/local/cuda/bin" >> ~/.bashrc
+sudo -H pip install -U jetson-stats
