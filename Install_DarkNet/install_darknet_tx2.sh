1e56101677b04f3f19e9b8ebb903bfc6d7441be0
JinFree <ytrqwe12@gmail.com>
2020-06-16 20:57:45 +0900

    darknet for jetson

--- Install_DarkNet/install_darknet_tx2.sh
+++ Install_DarkNet/install_darknet_tx2.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cp Makefile_tx2 ~/Makefile
+mv yolov3-tiny-prn.weights ~/yolov3-tiny-prn.weights
 cd
 git clone https://github.com/AlexeyAB/darknet.git
 cd darknet
 mv ../Makefile .
 make -j $(nproc)
+mkdir bin && cd bin && wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+mv ~/yolov3-tiny-prn.weights ~/darknet/bin/yolov3-tiny-prn.weights
