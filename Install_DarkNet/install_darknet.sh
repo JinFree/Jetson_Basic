085325f47c3efd103b028cc8d0d45807804f32f0
JinFree <ytrqwe12@gmail.com>
2020-06-16 20:44:13 +0900

    jetpack first

---
+++ Install_DarkNet/install_darknet.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd
+git clone https://github.com/AlexeyAB/darknet.git
+cd darknet
+
---
+++ Install_PyTorch/install_pytorch_1.5.0_jetpack_44.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd 
+wget https://nvidia.box.com/shared/static/3ibazbiwtkl181n95n9em3wtrca7tdzp.whl -O torch-1.5.0-cp36-cp36m-linux_aarch64.whl
+sudo apt-get install python3-pip libopenblas-base libopenmpi-dev 
+pip3 install Cython
+pip3 install numpy torch-1.5.0-cp36-cp36m-linux_aarch64.whl
+
+sudo apt-get install libjpeg-dev zlib1g-dev
+git clone --branch v0.6.0 https://github.com/pytorch/vision torchvision   # see below for version of torchvision to download
+cd torchvision
+sudo python3 setup.py install
+cd ../  # attempting to load torchvision from build dir will result in import error
