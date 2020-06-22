0423cfc1075895d1d0f069b28950f7d9fae59cbd
JinFree <ytrqwe12@gmail.com>
2020-06-22 14:38:11 +0900

    ready for venv

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt install python-dev python3-dev python-pip python3-pip -y
+sudo apt install python-dev python3-dev python-pip python3-pip python3-venv -y
 sudo -H pip install -U jetson-stats
