e7db85cc214734b5e6d4b95af5dfcc28e4e25e2e
JinFree <ytrqwe12@gmail.com>
2020-06-25 18:30:47 +0900

    tf_object_detection_api_install

--- Install_TensorFlow/install_tf1x_object_detection_api_jetpack_43.sh
+++ Install_TensorFlow/install_tf1x_object_detection_api_jetpack_43.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 cd 
-sudo apt-get install protobuf-compiler python3-pil python3-lxml python3-tk
-
-
+sudo apt-get install protobuf-compiler python3-pil python3-lxml python3-tk -y
+sudo apt install python3-matplotlib
+pip3 install Cython contextlib2 jupyter notebook tf_slim
+pip3 install pycocotools
+git clone https://github.com/tensorflow/models.git
+cd ~/models/research/
+protoc object_detection/protos/*.proto --python_out=.
+echo "export PYTHONPATH=\$PYTHONPATH:$(pwd):$(pwd)/slim" >> ~/.bashrc
+source ~/.bashrc
+python3 object_detection/builders/model_builder_tf1_test.py
 
