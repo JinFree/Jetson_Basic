1d69321d03552ca2601efea084d673d4de4025ce
JinFree <ytrqwe12@gmail.com>
2020-07-13 19:49:02 +0900

    weights

--- Install_DarkNet/install_darknet_tx2.sh
+++ Install_DarkNet/install_darknet_tx2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp Makefile_tx2 ~/Makefile
 cp yolov3-tiny-prn.weights ~/yolov3-tiny-prn.weights
+cp yolov3-tiny.weights ~/yolov3-tiny.weights
+cp yolov4-tiny.weights ~/yolov4-tiny.weights
 cd
 git clone https://github.com/AlexeyAB/darknet.git
 cd darknet
@@ -8,3 +10,5 @@ mv ../Makefile .
 make -j $(nproc)
 mkdir bin && cd bin && wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
 mv ~/yolov3-tiny-prn.weights ~/darknet/bin/yolov3-tiny-prn.weights
+mv ~/yolov3-tiny.weights ~/darknet/bin/yolov3-tiny.weights
+mv ~/yolov4-tiny.weights ~/darknet/bin/yolov4-tiny.weights
