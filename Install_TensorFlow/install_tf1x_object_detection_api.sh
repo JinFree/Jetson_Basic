0c47aa2ae196eb8e89a77b5aed4ca92e9e1035d1
JinFree <ytrqwe12@gmail.com>
2020-07-14 10:51:12 +0900

    jetpack tensorflow

--- Install_TensorFlow/install_tf1x_object_detection_api.sh
+++ Install_TensorFlow/install_tf1x_object_detection_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd 
 sudo apt-get install protobuf-compiler python3-pil python3-lxml python3-tk -y
-sudo apt install python3-matplotlib
+sudo apt install python3-matplotlib -y
 pip3 install Cython contextlib2 jupyter notebook tf_slim
 pip3 install pycocotools
 git clone https://github.com/tensorflow/models.git
