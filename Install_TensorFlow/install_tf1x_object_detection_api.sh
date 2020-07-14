157a18ee3a23360c214f49cce14568e5be85603b
JinFree <ytrqwe12@gmail.com>
2020-07-14 11:35:04 +0900

    fix error

--- Install_TensorFlow/install_tf1x_object_detection_api.sh
+++ Install_TensorFlow/install_tf1x_object_detection_api.sh
@@ -8,6 +8,7 @@ git clone https://github.com/tensorflow/models.git
 cd ~/models/research/
 protoc object_detection/protos/*.proto --python_out=.
 echo "export PYTHONPATH=\$PYTHONPATH:$(pwd):$(pwd)/slim" >> ~/.bashrc
-source ~/.bashrc
+export PYTHONPATH=\$PYTHONPATH:$(pwd):$(pwd)/slim
 python3 object_detection/builders/model_builder_tf1_test.py
+source ~/.bashrc
 
