9cfff73946ac0aac66d144838384bba0cd275322
JinFree <ytrqwe12@gmail.com>
2020-07-14 10:55:24 +0900

    darknet for jetpack44 remove cudnn

---
+++ Install_DarkNet/install_darknet_nano_jetpack_44.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+cp Makefile_nano_jetpack_44 ~/Makefile
+cp yolov3-tiny-prn.weights ~/yolov3-tiny-prn.weights
+cp yolov3-tiny.weights ~/yolov3-tiny.weights
+cp yolov4-tiny.weights ~/yolov4-tiny.weights
+cd
+git clone https://github.com/AlexeyAB/darknet.git
+cd darknet
+mv ../Makefile .
+make -j $(nproc)
+mkdir bin && cd bin && wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+mv ~/yolov3-tiny-prn.weights ~/darknet/bin/yolov3-tiny-prn.weights
+mv ~/yolov3-tiny.weights ~/darknet/bin/yolov3-tiny.weights
+mv ~/yolov4-tiny.weights ~/darknet/bin/yolov4-tiny.weights
