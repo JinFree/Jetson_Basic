d1a22af962d7760de028f8176e6924206de7c4ca
JinFree <ytrqwe12@gmail.com>
2020-07-21 19:30:34 +0900

    tensorflow for jetpack 4.2

---
+++ Install_TensorFlow/install_tensorflow_1x_jetpack_42.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd 
+sudo apt-get update
+sudo apt-get install libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev liblapack-dev libblas-dev gfortran -y
+
+pip3 install -U pip testresources setuptools wheel
+pip3 install -U numpy==1.16.1 future==0.17.1 mock==3.0.5 h5py==2.9.0 keras_preprocessing==1.0.5 keras_applications==1.0.8 gast==0.2.2 futures protobuf pybind11
+pip3 install --pre --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v42 'tensorflow<2'
