b5bfaf77bb571425e984d1bbd0f51a6b9c96aa01
JinFree <ytrqwe12@gmail.com>
2020-07-24 17:16:01 +0900

    jetson onboard cam

---
+++ onboard_cam.py
@@ -0,0 +1,52 @@
+#jetson tx2 onboard camera gstreamer string
+#gst_str = ("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv flip-method=0 ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+
+import cv2
+import numpy as np
+def imageProcessing(input):
+    output = np.copy(input)
+    return output
+
+def Video(openpath, savepath = None):
+    cap = cv2.VideoCapture(openpath)
+    if cap.isOpened():
+        print("Video Opened")
+    else:
+        print("Video Not Opened")
+        print("Program Abort")
+        exit()
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fourcc = int(cap.get(cv2.CAP_PROP_FOURCC))
+    out = None
+    if savepath is not None:
+        out = cv2.VideoWriter(savepath, fourcc, fps, (width, height), True)
+    cv2.namedWindow("Input", cv2.WINDOW_GUI_EXPANDED)
+    cv2.namedWindow("Output", cv2.WINDOW_GUI_EXPANDED)
+
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        if ret:
+            # Our operations on the frame come here
+            output = imageProcessing(frame)
+            # Write frame-by-frame
+            if out is not None:
+                out.write(output)
+            # Display the resulting frame
+            cv2.imshow("Input", frame)
+            cv2.imshow("Output", output)
+        else:
+            break
+        # waitKey(int(1000.0/fps)) for matching fps of video
+        if cv2.waitKey(int(1000.0/fps)) & 0xFF == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    if out is not None:
+        out.release()
+    cv2.destroyAllWindows()
+    return
+gst_str = ("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1280, height=(int)720, format=(string)NV12, framerate=(fraction)60/1 ! nvvidconv flip-method=0 ! video/x-raw, width=(int)1280, height=(int)720, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink")
+Video(gst_str)
