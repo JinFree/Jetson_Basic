a9edc1beb37b7f5e1cd0b37b576c11b4d5cacadb
JinFree <ytrqwe12@gmail.com>
2020-09-03 20:21:04 +0900

    for jetpack4.4

---
+++ Install_ONNX_Runtime/install_onnx_runtime_1.4.0_jetpack_44.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd 
+wget https://nvidia.box.com/shared/static/8sc6j25orjcpl6vhq3a4ir8v219fglng.whl -O onnxruntime_gpu-1.4.0-cp36-cp36m-linux_aarch64.whl
+
+# Install pip wheel
+pip3 install onnxruntime_gpu-1.4.0-cp36-cp36m-linux_aarch64.whl
---
+++ Install_PyTorch/install_pytorch_1.6.0_jetpack_44.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd 
+wget https://nvidia.box.com/shared/static/yr6sjswn25z7oankw8zy1roow9cy5ur1.whl -O torch-1.6.0rc2-cp36-cp36m-linux_aarch64.whl
+pip3 install Cython wheelnumpy
+pip3 install torch-1.6.0rc2-cp36-cp36m-linux_aarch64.whl
+
+
+sudo apt-get install libjpeg-dev zlib1g-dev
+git clone --branch v0.6.0 https://github.com/pytorch/vision torchvision
+cd torchvision
+sudo python3 setup.py install
+cd ../  # attempting to load torchvision from build dir will result in import error
