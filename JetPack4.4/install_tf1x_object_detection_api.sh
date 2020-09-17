f2322dd3ba604698c3507496935019f42bc1bdfd
JinFree <ytrqwe12@gmail.com>
2020-09-18 01:43:18 +0900

    tf_obj

--- JetPack4.4/install_tf1x_object_detection_api.sh
+++ JetPack4.4/install_tf1x_object_detection_api.sh
@@ -7,3 +7,4 @@ protoc object_detection/protos/*.proto --python_out=.
 # Install TensorFlow Object Detection API.
 cp object_detection/packages/tf1/setup.py .
 python3 -m pip install --use-feature=2020-resolver .
+python3 object_detection/builders/model_builder_tf1_test.py
