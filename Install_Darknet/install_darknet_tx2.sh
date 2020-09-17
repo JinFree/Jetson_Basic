19e6f4ce53692fa3924d5285cf811545121f4b7f
JinFree <ytrqwe12@gmail.com>
2020-09-17 16:20:25 +0900

    edit

--- Install_Darknet/install_darknet_tx2.sh
+++ Install_Darknet/install_darknet_tx2.sh
@@ -8,7 +8,7 @@ git clone https://github.com/JinFree/darknet.git
 cd darknet
 mv ../Makefile .
 make -j $(nproc)
-mkdir bin && cd bin && wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
+cd bin && wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
 mv ~/yolov3-tiny-prn.weights ~/darknet/bin/yolov3-tiny-prn.weights
 mv ~/yolov3-tiny.weights ~/darknet/bin/yolov3-tiny.weights
 mv ~/yolov4-tiny.weights ~/darknet/bin/yolov4-tiny.weights
