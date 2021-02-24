57f1b2bb908fe3966902712130c0fc47dfec54f2
JinFree <ytrqwe12@gmail.com>
2021-02-24 15:25:52 +0900

    onnx

---
+++ JetPack4.5/install_onnxruntime.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd 
+wget https://nvidia.box.com/shared/static/49fzcqa1g4oblwxr3ikmuvhuaprqyxb7.whl -O onnxruntime_gpu-1.6.0-cp36-cp36m-linux_aarch64.whl
+pip3 install onnxruntime_gpu-1.6.0-cp36-cp36m-linux_aarch64.whl
