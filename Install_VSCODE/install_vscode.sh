a45cbe5e7a9393457938684f824247be1f8eed2c
JinFree <ytrqwe12@gmail.com>
2023-11-12 10:12:41 +0900

    vscode

--- Install_VSCODE/install_vscode.sh
+++ Install_VSCODE/install_vscode.sh
@@ -3,6 +3,3 @@ cd
 git clone https://github.com/JetsonHacksNano/installVSCode.git
 cd installVSCode
 ./installVSCodeWithPython.sh
-# echo "alias VSCODE=code-oss" >> ~/.bashrc
-#!/bin/sh
-# source ~/.bashrc
